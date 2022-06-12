0d404699d545bb54586ec106a9abb2616be0cc42
Cobianzo <cobianzo@gmail.com>
2022-06-12 10:04:57 +0200

    Adding support align, works with one line

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ed233c102bd5a4a2978d');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '95c6fac64e70fe209a7e');
